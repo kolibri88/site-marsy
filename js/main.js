43a7acf452cc8ea61362f6b388e63d497fe2a0c8
kolibri88 <kolibri88@mail.ru>
2024-04-04 16:55:34 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+    // плавная прокрутка
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parant().addClass('active');
+        $('.menu__mobile .menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+    // мобильное меню
+    $('.menu__burger').click(function() {
+        $('.menu__mobile .menu').toggle(500);
+        $(this).toggleClass('close');
+    })
+});
+
